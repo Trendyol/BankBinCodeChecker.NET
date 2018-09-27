0b316dc99309c3d2883527c04e5308d0093c5d2f
Baris Ceviz <me@barisceviz.com>
2018-09-27 21:22:56 +0300

    Created constants

---
+++ src/BankBinCodeChecker/Constants.cs
@@ -0,0 +1,10 @@
+namespace BankBinCodeChecker
+{
+    public static class Constants
+    {
+        public const string BaseUrl = "";
+        public const string NotFoundMessage = "Bank bin code is not found";
+        public const string InvalidCodeMessage = "Bank bin code is checking invalid";
+        public const string UnknownErrorMessage = "Something went wrong when bank bin code is checking";
+    }
+}
